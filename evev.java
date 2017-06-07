6f7e7fec20c1a472c50bcc2b8bf98a989a0ca494
karthick1007 <karthickroshan6@gmail.com>
2017-06-07 15:12:41 +0530

    Create evev.java

---
+++ evev.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class eveno
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("even");
+}
+else
+{
+System.out.println("odd");
+}
+}
+}
